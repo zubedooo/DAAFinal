3964b8bd14b3be24fdd43c1f16800589a1d888ca
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-06-01 11:41:51 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,22 @@
+n=(input("Enter the number of men/women:\t"))
+men = dict([(raw_input('Enter the name of man:\t').strip(), raw_input('Enter the preference list:\n').split()[::-1]) for i in range(n)])
+women = dict([(raw_input('Enter the name of woman:\t').strip(), raw_input('Enter the preference list:\n').split()) for i in range(n)])
+pairs = {}
+not_engaged = set(men)
+while not_engaged:
+    man = not_engaged.pop()
+    if men[man] == []:
+        print('No stable match exists')
+        break
+    woman = men[man].pop()
+    if woman in pairs:
+        if women[woman].index(pairs[woman]) > women[woman].index(man):
+            not_engaged.add(pairs[woman])
+            pairs[woman] = man
+        else:
+            not_engaged.add(man)
+    else:
+        pairs[woman]=man
+else:
+    for i in pairs:
+        print (pairs[i],'is paired with',i)
